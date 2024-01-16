aeda148865b9d0a851101c06133e89efb0d7f243
mikiivanovic <mikiivanovic@icloud.com>
2024-01-16 14:14:46 +0000

    initial code stubs

---
+++ src/compiler.py
@@ -0,0 +1,6 @@
+class Compiler:
+    def __init__(self):
+        pass
+
+    def printHelloWorld(self):
+        print("hello world")
---
+++ tbc.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3
+
+from src import Compiler
+
+if __name__ == "__main__":
+    compiler = Compiler()
+    compiler.printHelloWorld()
